b31d0925c439a434edd53c34b886c286725ab6aa
Haris Spahija <haris.spahija@hybrit.org>
2020-05-06 12:05:36 +0200

    added a more meaningfull startup

--- src/startup.js
+++ src/startup.js
@@ -2,7 +2,7 @@
 // This can be for example setting up a collection or seeding data
 // Note that this fuction wil run every single time when starting the project up with docker-compose in dev mode.
 import Logger from "@reactioncommerce/logger";
-
+import seedExampleTemplateCollection from "./util/seedExampleTemplateCollection.js"
 /**
  * @name startup
  * @summary Called on startup
@@ -11,5 +11,11 @@ import Logger from "@reactioncommerce/logger";
  */
 export default async function pluginTemplateStartup(context) {
     Logger.info("Reaction-Plugin-Template startup function started")
+
+    // We can find our collection by looking into context
+    const { ExampleTemplateCollection } = context.collections;
+
+    // We will seed the data on every startup to make sure all of our translation files are added
+    await seedExampleTemplateCollection(ExampleTemplateCollection)
   }
   
