b8ab36f4e0afe9a22a384f1682624c3a5077206f
Haris Spahija <haris.spahija@hybrit.org>
2020-05-08 14:23:56 +0200

    updated jsdoc

--- src/graphql/resolvers/Query/hybrit.js
+++ src/graphql/resolvers/Query/hybrit.js
@@ -5,10 +5,10 @@
  * @summary query hybrit and return its slogan
  * @param {Object} _ - unused
  * @param {Object} args - an object of all arguments that were sent by the client
- * @param {String} args.shopId - id of user to query
+ * @param {String} args.language - language that needs to be selected
  * @param {Object} context - an object containing the per-request state
  * @param {Object} info Info about the GraphQL request
- * @returns {Promise<Object>} user account object
+ * @returns {Promise<Object>} slogan and language
  */
 export default async function hybrit(_, args, context, info) {
     const { language } = args;
